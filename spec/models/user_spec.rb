8c5e55baef0172d3f7a16e16d8ef2fa199e9f5cc
藤平 <taichi.f13213@gmail.com>
2025-06-06 18:21:45 +0900

    Request Spec を書こう【index 編】 Request Spec を書こう【show 編】 Request Spec を書こう【create 編】 Request Spec を書こう【update 編】 Request Spec を書こう【destroy 編】

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,8 +27,8 @@ RSpec.describe User, type: :model do
   # end
 
   context "すでに同じ名前の account が存在しているとき" do
+      before { create(:user,account: "foo") }
     it "ユーザー作成に失敗する" do
-      create(:user,account: "foo")
       # binding.pry
       user = build(:user,account: "foo")
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "ユーザーの一覧が取得できる" do
+    subject { get(users_path)}
+    before { create_list(:user, 3) }
+
+   it "ユーザーの一覧が取得できる" do
+
+      subject
+      # binding.pry
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["account", "name", "email"]
+      expect(response).to have_http_status(200)
+      # get users_path
     end
   end
+
+  describe "GET /users/:id" do
+    subject { get(user_path(user_id) )}
+    context "指定したidのユーザーが存在しているとき" do
+      let(:user_id) { user.id }
+      let(:user) { create(:user) }
+
+     it "そのユーザーのレコードが取得できる" do
+      subject
+      # binding.pry
+
+      res = JSON.parse(response.body)
+      expect(res["name"]).to eq user.name
+      expect(res["account"]).to eq user.account
+      expect(res["email"]).to eq user.email
+
+      expect(response).to have_http_status(200)
+     end
+   end
+
+    context "指定したidのユーザーが存在しないとき" do
+      let(:user_id){ 10000 }
+      it "ユーザーが見つからない" do
+        # binding.pry
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+ end
 
   describe "GET /users/:id" do
     it "任意のユーザーのレコードが取得できる" do
@@ -12,19 +51,64 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "POST /users" do
-    it "ユーザーのレコードが作成できる" do
+    subject { post(users_path, params: params) }
+
+   context "適切なパラメーターを送信したとき" do
+     let(:params) do
+       {user: attributes_for(:user) }
+     end
+     it "ユーザーのレコードが作成できる" do
+       expect { subject }.to change { User.count }.by(1)
+        # binding.pry
+        res = JSON.parse(response.body)
+        expect(res["name"]).to eq params[:user][:name]
+        expect(res["account"]).to eq params[:user][:account]
+        expect(res["email"]).to eq params[:user][:email]
+
+        expect(response).to have_http_status(200)
+      end
+   end
+
+   context "不適切な送信したパラメーターを送信したとき" do
+     let(:params)  { attributes_for(:user) }
+
+     it "エラーする" do
+       #  binding.pry
+       expect { subject }.to raise_error(ActionController::ParameterMissing)
+     end
     end
   end
 
   describe "PATCH /users/:id" do
-    it "任意のユーザーのレコードを更新できる" do
+    subject { patch(user_path(user_id), params: params)}
+
+    let(:params) do
+        { user: { name: Faker::Name.name, created_at: 1.day.ago } }
     end
+    let(:user_id) { user.id }
+    let(:user) { create(:user) }
+
+    it "任意のユーザーのレコードを更新できる" do
+      expect { subject }.to change { user.reload.name }.from(user.name).to(params[:user][:name])
+      expect { subject }.to not_change { user.reload.account }
+      expect { subject }.to not_change { user.reload.email }
+      expect { subject }.to not_change { user.reload.created_at }
+
+    end
+
   end
 
   describe "DELETE /users/:id" do
+    subject { delete(user_path(user_id)) }
+    let(:user_id) { user.id }
+    let!(:user) { create(:user) }
+
     it "任意のユーザーのレコードを削除できる" do
+     expect { subject }.to change { User.count }.by(-1)
+
     end
   end
+end
   # describe "GET /users" do
   #   it "works! (now write some real specs)" do
   #     get users_path
@@ -62,5 +146,3 @@ RSpec.describe "Users", type: :request do
   #     expect(response).to have_http_status(200)
   #   end
   # end
-
-end
